524f449f8a9cef25ea509ffd5e77c4de3ec27ee4
Miriam Lee <mimibaari+githubmimiAsusIII@gmail.com>
2016-02-03 17:59:20 -0600

    [PASSED] Week 2 - Exercise 1 Score 100 / 100 points earned PASSED Gradient Descent (for One Variable) ... ==                                   Part Name |     Score | Feedback ==                                   --------- |     ----- | -------- ==                            Warm-up Exercise |  10 /  10 | Nice work! ==           Computing Cost (for One Variable) |  40 /  40 | Nice work! ==         Gradient Descent (for One Variable) |  50 /  50 | Nice work! ==                       Feature Normalization |   0 /   0 | ==     Computing Cost (for Multiple Variables) |   0 /   0 | ==   Gradient Descent (for Multiple Variables) |   0 /   0 | ==                            Normal Equations |   0 /   0 | ==                                   -------------------------------- ==                                             | 100 / 100 |

--- ex1/gradientDescent.m
+++ ex1/gradientDescent.m
@@ -16,12 +16,25 @@ for iter = 1:num_iters
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCost) and gradient here.
     %
-
-
-
-
-
-
+    t0 = 0;
+    t1 = 0;
+    
+    %update theta 0
+    for i=1:m
+        h=theta(1)+theta(2)*X(i,2);
+        t0=t0+(h- y(i))*X(i,1);
+    end
+    t0=theta(1)-alpha*t0/m;
+    
+    %update theta 1
+    for i=1:m
+        h=theta(1)+theta(2)*X(i,2);
+        t1=t1+(h- y(i))*X(i,2);
+    end
+    t1=theta(2)-alpha*t1/m;
+    
+    theta(1)=t0
+    theta(2)=t1
 
     % ============================================================
 
