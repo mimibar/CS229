785c1d412f9f9a6fcb813fb61e43b1ce1d13f274
Miriam Lee <mimibaari+githubmimiAsusIII@gmail.com>
2016-02-03 17:02:02 -0600

    [partial] Week 2 - Exercise 1 Computing Cost (for One Variable) ... ==                                   Part Name |     Score | Feedback ==                                   --------- |     ----- | -------- ==                            Warm-up Exercise |  10 /  10 | Nice work! ==           Computing Cost (for One Variable) |  40 /  40 | Nice work! ==         Gradient Descent (for One Variable) |   0 /  50 | ==                       Feature Normalization |   0 /   0 | ==     Computing Cost (for Multiple Variables) |   0 /   0 | ==   Gradient Descent (for Multiple Variables) |   0 /   0 | ==                            Normal Equations |   0 /   0 | ==                                   -------------------------------- ==                                             |  50 / 100 |

--- ex1/computeCost.m
+++ ex1/computeCost.m
@@ -12,9 +12,11 @@ J = 0;
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the cost of a particular choice of theta
 %               You should set J to the cost.
-
-
-
+for i=1:m
+    h=theta(1)+theta(2)*X(i,2);
+    J=J+(h- y(i))^2;
+end
+J = J/(2*m)
 
 
 % =========================================================================
