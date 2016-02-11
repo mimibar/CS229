7a0361a983e92ed72c32cd36982313a6b74da538
Miriam Lee <mimibaari+githubmimiAsusIII@gmail.com>
2016-02-11 12:24:49 -0600

    [85/100] Week 3 - Exercise 2                         Sigmoid Function |   5 /   5 | Nice work!                 Logistic Regression Cost |  30 /  30 | Nice work!             Logistic Regression Gradient |  30 /  30 | Nice work!                                  Predict |   5 /   5 | Nice work!     Regularized Logistic Regression Cost |   0 /  15 | Regularized Logistic Regression Gradient |  15 /  15 | Nice work!                                          |  85 / 100 |

--- ex2/costFunctionReg.m
+++ ex2/costFunctionReg.m
@@ -17,6 +17,29 @@ grad = zeros(size(theta));
 %               Compute the partial derivatives and set grad to the partial
 %               derivatives of the cost w.r.t. each parameter in theta
 
+t=length(theta);
+% H (a vector of all the hypothesis values for the entire training set) as 
+% X * theta, with dimensions of (m x 1).
+H=X*theta ;
+s=sigmoid(H);
+
+% Cost & Gradient
+% you should not be regularizing
+% the theta(1) parameter (which corresponds to 0) in the code
+j=1;
+for i=1:m
+    J=J -y(i)*log(s(i))-(1-y(i))*log(1-s(i));
+    grad(j)= grad(j)+ (s(i)-y(i))*X(i,j);
+end
+J=J/m;
+grad(j)= grad(j)/m ;
+
+for j=2:t
+    for i=1:m
+        grad(j)= grad(j)+ (s(i)-y(i))*X(i,j);
+    end
+    grad(j)= grad(j)/m + (lambda/m)*theta(j);
+end
 
 
 
