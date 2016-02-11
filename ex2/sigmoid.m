fb475a0d70b361239d39469c1549b2ffa9983a92
Miriam Lee <mimibaari+githubmimiAsusIII@gmail.com>
2016-02-10 23:04:50 -0600

    [5/100] Week 3 - Exercise 2 Sigmoid Function |   5 /   5 | Nice work!

--- ex2/sigmoid.m
+++ ex2/sigmoid.m
@@ -8,11 +8,18 @@ g = zeros(size(z));
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the sigmoid of each value of z (z can be a matrix,
 %               vector or scalar).
-
-
-
-
-
+[m,n] = size(z);
+for i=1:m
+    e=exp(z(i,:));
+    g(i,:) = arrayfun(@(x) ex(x),e);
+    
 % =============================================================
+    
+end
+end
+
+function g = ex(z)
+
+g=1/(1+1/z);
 
 end
