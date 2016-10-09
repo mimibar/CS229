329c1c23d52a5d80a9faa295386bbec22122390f
Miriam Lee <mimibaari+githubmimiAsusIII@gmail.com>
2016-10-08 22:49:37 -0500

    [50 / 100] Week 7 - Exercise 6
    Programming Assignment: Support Vector Machines
    ==== Processed Email ====
    
    the quick brown fox jump over the lazi dog
    
    =========================
    ==
    ==                                   Part Name |     Score | Feedback
    ==                                   --------- |     ----- | --------
    ==                             Gaussian Kernel |  25 /  25 | Nice work!
    ==         Parameters (C, sigma) for Dataset 3 |  25 /  25 | Nice work!
    ==                         Email Preprocessing |   0 /  25 |
    ==                    Email Feature Extraction |   0 /  25 |
    ==                                   --------------------------------
    ==                                             |  50 / 100 |
    ==

--- ex6/dataset3Params.m
+++ ex6/dataset3Params.m
@@ -22,13 +22,27 @@ sigma = 0.3;
 %  Note: You can compute the prediction error using 
 %        mean(double(predictions ~= yval))
 %
+p=[0.01 0.03 0.1 0.3 1 3 10 30];
+min=flintmax;
 
-
-
-
-
-
-
+for i=1:8
+    c=p(i);
+    for j=1:8
+        s=p(j);
+        
+        model= svmTrain(X, y, c, @(x1, x2) gaussianKernel(x1, x2, s));
+ 
+        %evaluating (on the cross validation set)
+        predictions = svmPredict(model, Xval);
+        
+        error=mean(double(predictions ~= yval));
+        if error<min
+            min=error;
+            C=c;
+            sigma=s;
+        end
+    end
+end
 % =========================================================================
 
 end
